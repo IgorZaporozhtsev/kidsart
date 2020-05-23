98d5defd9f49050d7f5e0db6e5d432e942b50198
Igor Zaporogtsev <igorzaporogtsev@gmail.com>
2020-05-23 12:37:07 +0300

    initial commit

---
+++ src/main/java/com/zeecoder/kidsart/KidsartApplication.java
@@ -0,0 +1,13 @@
+package com.zeecoder.kidsart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeecoder/kidsart/KidsartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeecoder.kidsart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
