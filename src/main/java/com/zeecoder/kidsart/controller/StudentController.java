981d8417b78994ba9b4f6cca6b780c7528dac0b8
Igor Zaporogtsev <igorzaporogtsev@gmail.com>
2020-05-23 17:21:19 +0300

    add init controller

---
+++ src/main/java/com/zeecoder/kidsart/controller/StudentController.java
@@ -0,0 +1,22 @@
+package com.zeecoder.kidsart.controller;
+
+import com.zeecoder.kidsart.model.Student;
+import com.zeecoder.kidsart.model.Student.Gender;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+
+  @GetMapping
+  public List<Student> getAllStudents(){
+    return List.of(
+          new Student(UUID.randomUUID(), "James", "Bond", "jamesbons@gmail.com", Student.Gender.MALE),
+          new Student(UUID.randomUUID(), "Elisa", "Tamara", "elisatamara@gmail.com", Gender.FEMALE)
+        );
+  }
+}
