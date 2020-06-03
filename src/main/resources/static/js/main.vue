acccf37c78115099d232279fefc0822144bfc0bd
zeecoder <igorzaporogtsev@gmail.com>
2020-06-03 10:07:34 +0300

    add Message Controller add Vue message

---
+++ src/main/resources/static/js/main.vue
@@ -0,0 +1,85 @@
+
+var messageApi = Vue.resource('/message{/id}')
+
+Vue.component('message-form', {
+    props: ['messagesAttr'],
+
+    data: function() {
+        return {
+            text: ''
+        }
+    },
+
+    template:
+        '<div>' +
+            '<input type="text" placeholder="Write something" v-model="text" />' + 
+            '<input type="button" value="Save" @click="save"/>' + 
+        '</div>',
+    
+    methods: {
+        save: function() {
+            var message = { text: this.text };
+
+            messageApi.save({}, message).then(result =>
+                result.json().then(data => {
+                    this.messages.push(data);
+                    this.text = ''
+                })
+            )
+        }
+    }    
+});
+
+Vue.component('message-row', {
+    props: ['message'],
+    template: 
+        '<div>' + 
+            '<i>({{ message.id }})</i>{{ message.text }}' +
+            '<span>' +
+                '<input type="button" value="Edit" @click="edit"/>' +
+            '</span>' + 
+         '</div>',
+    methods: {
+        edit : function() {
+            this.editMethod(this.message);
+        }
+    }
+});
+
+Vue.component('messages-list', {
+    props:['messages'],
+    data: function(){
+        return {
+            message: null
+        }
+    },
+
+    template: '<div>' + 
+                    '<message-form :messages="messages" :messageAttr="message" />' +
+                    '<message-row v-for="message in messages" :key="message.id" :message="message" :editMethod="editMethod" />'+
+               '</div>',
+    
+    created: function(){
+        messageApi.get().then(result =>
+            result.json().then(data =>
+                data.forEach(message => this.messages.push(message))
+            )
+        )
+    },
+
+    methods: {
+        editMessage: function(){
+            this.message = message;
+        }
+    }
+});
+
+var app = new Vue({
+    el: '#app',
+    template: '<messages-list :messages="messages"/>',
+    data: {
+        messages: [
+            
+        ]
+    }
+});
